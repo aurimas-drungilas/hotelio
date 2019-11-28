cc3cf93d4c13d4a50a6b2228a06e5f92167b357f
Aurimas Drungilas <aurimas.drungilas@gmail.com>
2019-11-28 21:30:05 +0000

    Adding some data to dataloader

---
+++ back-end/src/main/java/com/codeclan/hotelio/components/DataLoader.java
@@ -0,0 +1,49 @@
+package com.codeclan.hotelio.components;
+
+import com.codeclan.hotelio.models.Booking;
+import com.codeclan.hotelio.models.Customer;
+import com.codeclan.hotelio.models.Room;
+import com.codeclan.hotelio.repositories.BookingRepository;
+import com.codeclan.hotelio.repositories.CustomerRepository;
+import com.codeclan.hotelio.repositories.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+import java.util.Date;
+
+public class DataLoader implements ApplicationRunner {
+    @Autowired
+    BookingRepository bookingRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+    @Autowired
+    RoomRepository roomRepository;
+
+    public DataLoader() {
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Room room404 = new Room(404, 2);
+        roomRepository.save(room404);
+
+        Room room405 = new Room(405, 2);
+        roomRepository.save(room405);
+
+        Room room406 = new Room(406, 2);
+        roomRepository.save(room406);
+
+        Room room407 = new Room(407, 4);
+        roomRepository.save(room407);
+
+        Customer customerNiall = new Customer("Niall", "Morris", 26);
+        customerRepository.save(customerNiall);
+
+        Customer customerJordan = new Customer("Jordan", "Davidson", 29);
+        customerRepository.save(customerJordan);
+
+        Booking booking1 = new Booking("21/11/2019", "27/11/2019", 1);
+        bookingRepository.save(booking1);
+    }
+}
