334f2ffff76d9b1e6dbe1aec31a9a019a0ad42d2
Aurimas Drungilas <aurimas.drungilas@gmail.com>
2019-11-28 20:24:38 +0000

    Importing generated spring project and setting up defaults

---
+++ back-end/src/main/java/com/codeclan/hotelio/HotelioApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.hotelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelioApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/codeclan/hotelio/HotelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.hotelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
