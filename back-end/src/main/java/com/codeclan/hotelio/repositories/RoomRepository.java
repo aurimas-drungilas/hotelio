fb333b9dd84b24ea208cf1f54957365f9713c600
Aurimas Drungilas <aurimas.drungilas@gmail.com>
2019-11-28 21:05:28 +0000

    Creating the rest repositories

---
+++ back-end/src/main/java/com/codeclan/hotelio/repositories/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.codeclan.hotelio.repositories;
+
+import com.codeclan.hotelio.models.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
