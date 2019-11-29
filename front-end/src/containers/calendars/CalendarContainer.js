28846a9c8987b4e329d328ee5fbf465142e3e573
Aurimas Drungilas <aurimas.drungilas@gmail.com>
2019-11-29 21:00:12 +0000

    Adding Timeline view with resources

--- front-end/src/containers/calendars/CalendarContainer.js
+++ front-end/src/containers/calendars/CalendarContainer.js
@@ -4,28 +4,81 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from '@fullcalendar/list';
+import resourceDayGridPlugin from '@fullcalendar/resource-daygrid';
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
 
 import "@fullcalendar/core/main.css";
 import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timeline/main.css";
+import "@fullcalendar/resource-timeline/main.css";
 
 
 class CalendarContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {  
-            defaultView: 'dayGridMonth',
+            // @Docs: https://fullcalendar.io/docs/timeline-view
+            defaultView: 'resourceTimelineMonth',
             // @Docs: https://fullcalendar.io/docs/header
             header: {
                 left: 'today prev next',
                 center: 'title',
-                right: 'dayGridMonth listMonth dayGridWeek'
+                right: 'resourceTimelineMonth dayGridMonth listMonth dayGridWeek'
             },
             // @Docs: https://fullcalendar.io/docs/event-object
             events: [
-                { id: 1, title: 'Jordan\'s booking', start: '2019-11-06', end: '2019-11-09', allDay: true },
-                { id: 2, title: 'Nial\'s booking', start: '2019-11-07', end: '2019-11-07', allDay: true },
-                { id: 3, title: 'Nial\'s 2nd booking', start: '2019-11-25', end: '2019-12-05', allDay: true }
-            ]
+                { 
+                    id: 1, 
+                    resourceId: 1, 
+                    title: 'Jordan\'s booking', 
+                    start: '2019-09-01', 
+                    end: '2019-11-09', 
+                    allDay: true 
+                },
+                { 
+                    id: 2, 
+                    resourceId: 3, 
+                    title: 'Nial\'s booking', 
+                    start: '2019-11-02', 
+                    end: '2019-11-25', 
+                    allDay: true 
+                },
+                { 
+                    id: 3, 
+                    resourceId: 3, 
+                    title: 'Nial\'s 2nd booking', 
+                    start: '2019-11-03', 
+                    end: '2019-12-09', 
+                    allDay: true 
+                }
+            ],
+            resources: [
+                {
+                    id: 1,
+                    title: "Room 404",
+                    capacity: 2
+                },
+                {
+                    id: 2,
+                    title: "Room 405",
+                    capacity: 2
+                },
+                {
+                    id: 3,
+                    title: "Room 406",
+                    capacity: 4
+                }
+            ],
+            resourceColumns: [
+                {
+                  labelText: 'Room',
+                  field: 'title'
+                },
+                {
+                  labelText: 'Capacity',
+                  field: 'capacity'
+                }
+              ]
         }
     }
 
@@ -37,6 +90,8 @@ class CalendarContainer extends Component {
                 events: this.state.events.concat({
                     // creates a new array
                     title: "New Booking",
+                    // TODO: look up how to get resourceId from the arg
+                    resourceId: 2, 
                     start: arg.date,
                     end: arg.date,
                     allDay: true
@@ -56,11 +111,12 @@ class CalendarContainer extends Component {
                 <FullCalendar 
                     defaultView={this.state.defaultView} 
                     header={this.state.header}
-                    plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
+                    plugins={[dayGridPlugin, interactionPlugin, listPlugin, resourceDayGridPlugin, resourceTimelinePlugin]}
                     events={this.state.events}
                     eventClick={this.handleEventClick}
                     dateClick={this.handleDateClick}
                     resources={this.state.resources}
+                    resourceColumns={this.state.resourceColumns}
                     />
             </div>
          );
