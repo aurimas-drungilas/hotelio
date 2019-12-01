2978f05c2b07908293bdc728660aa8c8ef3a28ba
Aurimas Drungilas <aurimas.drungilas@gmail.com>
2019-12-01 17:51:55 +0000

    Updating the BookingForm with live values

--- front-end/src/components/bookings/BookingForm.js
+++ front-end/src/components/bookings/BookingForm.js
@@ -1,39 +1,113 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Request from '../../helpers/request';
 
-const BookingForm = (props) => {
+class BookingForm extends React.Component {
 
-    const handleSubmit = (event) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            availableRooms: [],
+            availableGuests: []
+        };
+        this.handleNewBooking = this.handleNewBooking.bind(this);
+        this.populateAvailableRooms = this.populateAvailableRooms.bind(this);
+        this.populateAvailableGuests = this.populateAvailableGuests.bind(this);
+    }
+
+    componentDidMount() {
+        this.populateAvailableRooms();
+        this.populateAvailableGuests();
+    }
+
+    populateAvailableRooms() {
+        // TODO: only show actually available rooms.
+        // TODO: refresh this when the date gets updated.
+        const request = new Request();
+        const url = '/api/rooms?size=1000';
+        request.get(url)
+            .then(json => this.setState({availableRooms: json._embedded.rooms}));
+    }
+
+    populateAvailableGuests() {
+        const request = new Request();
+        const url = '/api/guests?size=1000';
+        request.get(url)
+            .then(json => this.setState({availableGuests: json._embedded.guests}));
+    }
+
+    handleNewBooking(event) {
         event.preventDefault();
-        const booking ={
-            "arrivalDate": event.target.arrivalDate.value,
-            "departureDate": event.target.departureDate.value,
-            "firstName": event.target.firstName.value,
-            "lastName": event.target.lastName.value,
-            "numberOfPeople": event.target.numberOfPeople.value,
-            "rooms": event.target.rooms.value
-        }
-        props.onNewBooking(booking);
-    
-      }
 
-    return ( 
-        <div className="booking-form">
-            <form onSubmit={handleSubmit}>
-                <span>Arrival date: </span>
-                <input type="date" name="arrivalDate" />
-                <span>Departure date: </span>
-                <input type="date" name="departureDate" />
-                <span>Guest: </span>
-                <input type="text" placeholder="First Name" name="firstName" />
-                <input type="text" placeholder="Last Name" name="lastName" />
-                <span>Number of People: </span>
-                <input type="number" name="numberOfPeople"/>
-                <span>Rooms: </span>
-                <input type="datalist" name="rooms" />
-                <button type="submit">Book</button>
-            </form>
-        </div>
-     );
+        const roomsSelected = [];
+        for (const room of event.target.rooms.selectedOptions) {
+            roomsSelected.push(room.value);
+        }
+
+        const booking = {
+            "guest": event.target.guest.value,
+            "startDate": event.target.startDate.value,
+            "endDate": event.target.endDate.value,
+            "numberOfPeople": event.target.numberOfPeople.value,
+            "rooms": roomsSelected
+        }
+        console.dir(booking);
+        this.props.onNewBooking(booking);
+    }
+
+    render() {
+        const availableRoomsOptions = this.state.availableRooms.map((room, index) => {
+            return(
+                <option key={index} value={room._links.self.href}>{room.roomNumber}</option>
+            );
+        });
+
+        const availableGuestsOptions = this.state.availableGuests.map((guest, index) => {
+            return(
+                <option key={index} value={guest._links.self.href}>{guest.firstName} {guest.lastName}</option>
+            );
+        });
+
+        return ( 
+            <div className="booking-form">
+                <form onSubmit={this.handleNewBooking}>
+                    <div className="booking-form__field">
+                        <p>Guest</p>
+                        <select name="guest" defaultValue={''} required>
+                            <option value={''} disabled={'disabled'}>Select a guest</option>
+                            <option value={'new'} disabled>New guest</option>
+                            {availableGuestsOptions}
+                        </select>
+                    </div>
+
+                    <div className="booking-form__field">
+                        <p>Arrival date</p>
+                        <input type="date" name="startDate" value="2019-12-02" required />
+                    </div>
+
+                    <div className="booking-form__field">
+                        <p>Departure date</p>
+                        <input type="date" name="endDate" value="2019-12-03" required />
+                    </div>
+
+                    <div className="booking-form__field">
+                        <p>Number of People</p>
+                        <input type="number" name="numberOfPeople" value="2" required />
+                    </div>
+
+                    <div className="booking-form__field">
+                        <p>Available rooms</p>
+                        <select name="rooms" size="5" multiple={true} required>
+                            {availableRoomsOptions}
+                        </select>
+                    </div>
+
+                    <div className="booking-form__submit">
+                        <button type="submit">Book</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
- 
+
 export default BookingForm;
