602725e437029231cfb75f7545c202d15ed14154
Aurimas Drungilas <aurimas.drungilas@gmail.com>
2019-12-01 14:15:50 +0000

    Importing Bootstrap4 into the project

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import MainContainer from './containers/MainContainer';
 import SidebarContainer from './containers/SidebarContainer';
 
