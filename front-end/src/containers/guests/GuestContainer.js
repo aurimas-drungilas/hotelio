f5d0fb1bc547bad6bd97aee6a152ddd719a6af9a
Aurimas Drungilas <aurimas.drungilas@gmail.com>
2019-12-02 10:48:19 +0000

    Adding create new guests form

--- front-end/src/containers/guests/GuestContainer.js
+++ front-end/src/containers/guests/GuestContainer.js
@@ -2,13 +2,16 @@ import React, { Component, Fragment } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Request from '../../helpers/request';
 import GuestList from '../../components/guests/GuestList';
+import GuestCreateForm from '../../components/guests/GuestCreateForm';
+
 
 class GuestContainer extends Component {
     constructor(props) {
         super(props);
         this.state = { 
             guests: []
-         }
+        }
+        this.handleCreateGuest = this.handleCreateGuest.bind(this);
     }
 
     componentDidMount() {
@@ -21,12 +24,22 @@ class GuestContainer extends Component {
         })
     }
 
-    render() { 
+    handleCreateGuest(guest) {
+        const request = new Request();
+        const url = '/api/guests';
+        request.post(url, guest)
+            .then(() => window.location = '/guests');
+    }
+
+    render() {
         return ( 
             <div className="component-container">
                 <Router>
                 <Fragment>
                     <Switch>
+                        <Route exact path="/guests/new" render={() => {
+                            return <GuestCreateForm onCreateGuest={this.handleCreateGuest} />
+                        }} />
                         <Route render={() => {
                             return <GuestList guests={this.state.guests} />
                         }} />
